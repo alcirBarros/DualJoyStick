30a9476dfc4488fb4048fbf6dcec2a5a250b8162
alci <alci@debian.home>
2016-09-13 05:14:53 -0300

    Commit 13/09/2016

--- JoyStick/src/com/selfsystem/activity/MainActivity.java
+++ JoyStick/src/com/selfsystem/activity/MainActivity.java
@@ -8,7 +8,6 @@ import com.selfsystem.joystick.R.layout;
 import com.selfsystem.joystick.R.menu;
 import com.selfsystem.proxy.rest.ProxyClienteRest;
 
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -16,40 +15,39 @@ import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
-	private DualJoyStickView dualJoyStickView;
-	private ProxyClienteRest proxyClienteRest = new ProxyClienteRest(); 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_main);
-		
-		final Button buttonA = (Button) findViewById(R.id.button_A);
-		final Button buttonB = (Button) findViewById(R.id.button_B);
-		final Button buttonC = (Button) findViewById(R.id.button_C);
-		
-		
-		dualJoyStickView = (DualJoyStickView) findViewById(R.id.dualjoystickView);
-		
-		dualJoyStickView.getJoyStickL().setOnJoystickMoveListener(new OnJoystickMoveListener() { 
-			public void onValueChanged(int angle, int power, int direction) {
-        	   proxyClienteRest.joyStickRest(angle, power, direction, "stickL"); 
+    private DualJoyStickView dualJoyStickView;
+    private ProxyClienteRest proxyClienteRest = new ProxyClienteRest();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonA = (Button) findViewById(R.id.button_A);
+        final Button buttonB = (Button) findViewById(R.id.button_B);
+        final Button buttonC = (Button) findViewById(R.id.button_C);
+
+        dualJoyStickView = (DualJoyStickView) findViewById(R.id.dualjoystickView);
+
+        dualJoyStickView.getJoyStickL().setOnJoystickMoveListener(new OnJoystickMoveListener() {
+            public void onValueChanged(int angle, int power, int direction) {
+                proxyClienteRest.joyStickRest(angle, power, direction, "stickL");
             }
         }, JoystickView.DEFAULT_LOOP_INTERVAL);
-		
-		dualJoyStickView.getJoyStickR().setOnJoystickMoveListener(new OnJoystickMoveListener() {
-           public void onValueChanged(int angle, int power, int direction) {
-        	   proxyClienteRest.joyStickRest(angle, power, direction, "stickR");         	   
+
+        dualJoyStickView.getJoyStickR().setOnJoystickMoveListener(new OnJoystickMoveListener() {
+            public void onValueChanged(int angle, int power, int direction) {
+                proxyClienteRest.joyStickRest(angle, power, direction, "stickR");
             }
         }, JoystickView.DEFAULT_LOOP_INTERVAL);
-	}
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
--- JoyStick/src/com/selfsystem/joystick/DualJoyStickView.java
+++ JoyStick/src/com/selfsystem/joystick/DualJoyStickView.java
@@ -11,74 +11,74 @@ import android.widget.LinearLayout.LayoutParams;
 
 public class DualJoyStickView extends LinearLayout {
 
-	private JoystickView joyStickL;
-	private JoystickView joyStickR;
+    private JoystickView joyStickL;
+    private JoystickView joyStickR;
 
-	private View pad;
+    private View pad;
 
-	Context context;
+    Context context;
 
-	public DualJoyStickView(Context context) {
-		super(context);
-		joyStickL = new JoystickView(context);
-		joyStickR = new JoystickView(context);
-		this.context = context;
-		initDualJoystickView();
-	}
+    public DualJoyStickView(Context context) {
+        super(context);
+        joyStickL = new JoystickView(context);
+        joyStickR = new JoystickView(context);
+        this.context = context;
+        initDualJoystickView();
+    }
 
-	public DualJoyStickView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		joyStickL = new JoystickView(context, attrs, false);
-		joyStickR = new JoystickView(context, attrs, true);
-		this.context = context;
-		initDualJoystickView();
-	}
+    public DualJoyStickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        joyStickL = new JoystickView(context, attrs, false);
+        joyStickR = new JoystickView(context, attrs, true);
+        this.context = context;
+        initDualJoystickView();
+    }
 
-	private void initDualJoystickView() {
+    private void initDualJoystickView() {
 
-		joyStickL.setbuttonColor(Color.rgb(0x70, 0x20, 0x20));
-		joyStickR.setbuttonColor(Color.rgb(0x20, 0x50, 0x30));
+        joyStickL.setbuttonColor(Color.rgb(0x70, 0x20, 0x20));
+        joyStickR.setbuttonColor(Color.rgb(0x20, 0x50, 0x30));
 
-		pad = new View(getContext());
-	}
+        pad = new View(getContext());
+    }
 
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		removeView(joyStickL);
-		removeView(joyStickR);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        removeView(joyStickL);
+        removeView(joyStickR);
 
-		float padW = getMeasuredWidth() - (getMeasuredHeight() * 2);
-		int joyWidth = (int) ((getMeasuredWidth() - padW) / 2);
-		LayoutParams joyLParams = new LayoutParams(joyWidth,
-				getMeasuredHeight());
+        float padW = getMeasuredWidth() - (getMeasuredHeight() * 2);
+        int joyWidth = (int) ((getMeasuredWidth() - padW) / 2);
+        LayoutParams joyLParams = new LayoutParams(joyWidth,
+                getMeasuredHeight());
 
-		joyStickL.setLayoutParams(joyLParams);
-		joyStickR.setLayoutParams(joyLParams);
+        joyStickL.setLayoutParams(joyLParams);
+        joyStickR.setLayoutParams(joyLParams);
 
-		addView(joyStickL);
-		ViewGroup.LayoutParams padLParams = new ViewGroup.LayoutParams(
-				(int) padW, getMeasuredHeight());
+        addView(joyStickL);
+        ViewGroup.LayoutParams padLParams = new ViewGroup.LayoutParams(
+                (int) padW, getMeasuredHeight());
 
-		removeView(pad);
+        removeView(pad);
 
-		pad.setLayoutParams(padLParams);
+        pad.setLayoutParams(padLParams);
 
-		addView(pad);
+        addView(pad);
 
-		addView(joyStickR);
+        addView(joyStickR);
 		//
-		// Button button = new Button(context);
-		// button.setText("Teste");
-		//
-		// addView(button);
-	}
+        // Button button = new Button(context);
+        // button.setText("Teste");
+        //
+        // addView(button);
+    }
 
-	public JoystickView getJoyStickL() {
-		return joyStickL;
-	}
+    public JoystickView getJoyStickL() {
+        return joyStickL;
+    }
 
-	public JoystickView getJoyStickR() {
-		return joyStickR;
-	}
+    public JoystickView getJoyStickR() {
+        return joyStickR;
+    }
 }
